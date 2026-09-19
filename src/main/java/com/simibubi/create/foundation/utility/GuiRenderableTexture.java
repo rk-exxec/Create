13802fa4e6e8a0bc73e7f7eb0a926eef63abd851
rk-exxec <40037381+rk-exxec@users.noreply.github.com>
2026-09-19 21:53:33 +0200

    added api func for location

--- src/main/java/com/simibubi/create/foundation/utility/GuiRenderableTexture.java
+++ src/main/java/com/simibubi/create/foundation/utility/GuiRenderableTexture.java
@@ -3,6 +3,7 @@ package com.simibubi.create.foundation.utility;
 import net.createmod.catnip.gui.TextureSheetSegment;
 import net.createmod.catnip.gui.element.ScreenElement;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.resources.ResourceLocation;
 
 public interface GuiRenderableTexture extends ScreenElement, TextureSheetSegment {
     void render(GuiGraphics graphics, int x, int y);
@@ -14,5 +15,6 @@ public interface GuiRenderableTexture extends ScreenElement, TextureSheetSegment
 	int getWidth();
 
 	int getHeight();
-    
+	
+    public ResourceLocation getLocation();
 }
